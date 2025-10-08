37cd00b2f7982acb861495f9ef70ce2b115984ff
Andrei Karshkov <karshkoff@gmail.com>
2025-10-09 00:06:30 +0200

    Initial commit

---
+++ dev.s3.backend.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "tf-backend" {
+  bucket        = "j-llm-tf-state-bucket-dev"
+  force_destroy = true
+
+  tags = {
+    project = "j-llm"
+    repo    = "https://github.com/karshkoff/j-llm-infra-bootstrap"
+  }
+}
+
+
+# Private by default
+resource "aws_s3_bucket_public_access_block" "tf-backend-private" {
+  bucket = aws_s3_bucket.tf-backend.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
